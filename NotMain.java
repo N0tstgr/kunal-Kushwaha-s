4a17c2e16f115274661b961e606ef41f04e2a6d9
N0tstgr <vikashkasaudhan321@gmail.com>
2025-08-28 22:37:10 +0530

    ks

---
+++ NotMain.java
@@ -0,0 +1,6 @@
+public class NotMain {
+    public static void main(String[] args){
+        System.out.println("Hello vikash");
+    }
+
+}
---
+++ zuxxy.java
@@ -0,0 +1,6 @@
+public class zuxxy {
+    public static void main(String[] args) {
+//        System.out.println();
+        System.out.println(args[0]);
+    }
+}
