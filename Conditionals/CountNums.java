db7ef9e7011355cf39b22f23a0159e47c5cc6b0a
N0tstgr <vikashkasaudhan321@gmail.com>
2025-09-01 22:33:12 +0530

    countNums

---
+++ Conditionals/CountNums.java
@@ -0,0 +1,18 @@
+package Conditionals;
+
+public class CountNums {
+    public static void main(String[] args) {
+        int n  = 8999;
+
+        int count = 0;
+        while (n>0 ){
+            int rem = n%10;
+            if(rem==5){
+                count++;
+            }
+            n = n/10;  // n /10 = 0
+
+        }
+        System.out.println(count);
+    }
+}
