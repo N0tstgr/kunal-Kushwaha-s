1ca87222d2619b37a54be5b5ea626c5fb16bc3f8
Your Name <vikashkasaudhan321@gmail.com>
2025-09-14 22:43:42 +0530

    EvenOdd.2

--- Not-Dsa/src/com/company/Evenodd_01.java
+++ Not-Dsa/src/com/company/Evenodd_01.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 public class Evenodd_01 {
     public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int a = sc.nextInt();
 
+
+            if(a%2==0){
+                System.out.println("this is even number");
+            }else{
+                System.out.println("This is odd number");
+            }
+        sc.close();
 
     }
 }
