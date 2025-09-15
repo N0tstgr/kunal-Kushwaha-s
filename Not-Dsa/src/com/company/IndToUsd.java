8712ed2103791ebabb37aec30a6e85f2f57ccd7a
Your Name <vikashkasaudhan321@gmail.com>
2025-09-15 21:54:32 +0530

    indToDollar

---
+++ Not-Dsa/src/com/company/IndToUsd.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class IndToUsd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the amount of indian rupee to convert into usd");
+        double rupee = sc.nextDouble();
+        double dollar = 0;
+        dollar = rupee/88.15;
+
+        System.out.println  ("this is the dollar version of indian rupee "  + dollar);
+
+
+
+
+    }
+}
