e35e52641f8c3b044b0bda3d7592f4db155fb6d3
Your Name <vikashkasaudhan321@gmail.com>
2025-09-15 21:36:28 +0530

    CAlculator

--- Not-Dsa/src/com/company/Calculator_04.java
+++ Not-Dsa/src/com/company/Calculator_04.java
@@ -14,12 +14,32 @@ public class Calculator_04 {
         System.out.println("Enter the number  to which you want to perform the operation");
         double b = sc.nextDouble();
 
-        double result;
+        double result = 0;
 
         switch(op){
 
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a-b;
+                break;
+            case '*':
+                result = a*b;
+                break;
+            case '/':
+               if(b!=0){
+                   result = a/b;
+               }else{
+                   System.out.println("There is an error try another input");
+               }
+               break;
+
+            default:
+                System.out.println("This is the out of the range so it is not processable ");
+
         }
 
-
+        System.out.println(a +"  "+ op + " " + b + " " + " = "+  result);
     }
 }
