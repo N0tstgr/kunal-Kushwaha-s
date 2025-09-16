0971d9713f8592d969f3ed62b72919f4f0c65614
Your Name <vikashkasaudhan321@gmail.com>
2025-09-16 22:14:04 +0530

    fibo.2

--- Not-Dsa/src/com/company/Fibonacci_06.java
+++ Not-Dsa/src/com/company/Fibonacci_06.java
@@ -4,6 +4,10 @@ public class Fibonacci_06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number to be find the fibonacci ");
+        static int fibonacci(int n);
+        if (n<=0){
+            return n
+        }
 
     }
 }
