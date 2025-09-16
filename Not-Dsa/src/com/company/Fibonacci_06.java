687b064cec9e671c8825719c8b2467ced04ba4ae
Your Name <vikashkasaudhan321@gmail.com>
2025-09-16 22:36:49 +0530

    fibonacci.org

--- Not-Dsa/src/com/company/Fibonacci_06.java
+++ Not-Dsa/src/com/company/Fibonacci_06.java
@@ -1,13 +1,30 @@
 package com.company;
 import java.util.Scanner;
+
+class fibonacci{
+static int fibonacci(int n){
+    if (n==0)
+        return 0;
+    if(n==1)
+        return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
+
+}
+}
 public class Fibonacci_06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number to be find the fibonacci ");
-        static int fibonacci(int n);
-        if (n<=0){
-            return n
-        }
+
+        System.out.println("Enter the number to find the fibonacci number : ");
+        int a = sc.nextInt();
+
+        System.out.print("Fibonacci series  ");
+        fibonacci.fibonacci(6);
+
+    for(int i = 0; i<=a; i++){
+        System.out.print( fibonacci.fibonacci(i) + " ");
+    }
+
 
     }
 }
