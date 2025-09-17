44dea18654357cb2ed26c24020d8acb0ccff0911
Your Name <vikashkasaudhan321@gmail.com>
2025-09-17 23:14:39 +0530

    palindrome

--- Not-Dsa/src/com/company/palindrom_07.java
+++ Not-Dsa/src/com/company/palindrom_07.java
@@ -1,7 +1,17 @@
 package com.company;
+import java.util.Scanner;
+
+public class CheckPalindrome{
+    public static boolean isPalindromeNumber(long n){
+
+    }
+
+}
+
 
 public class palindrom_07 {
     public static void main(String[] args) {
-
+Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number you are looking for the palindrome");
     }
 }
