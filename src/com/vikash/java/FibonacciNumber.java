40c3ce59641caeee57fe0cdea0bef942700df8ff
N0tstgr <vikashkasaudhan321@gmail.com>
2026-01-20 11:24:28 +0530

    sc

--- src/com/vikash/java/FibonacciNumber.java
+++ src/com/vikash/java/FibonacciNumber.java
@@ -2,15 +2,17 @@ package com.vikash.java;
 import java.util.Scanner;
 public class FibonacciNumber {
     static void main() {
-        int b,c;
+        int b=0;
+        int c =0;
 
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        for(int i = 1; i<=a; i++){
-            b = i;
+         for(int i = 1; c<=a; i++){
+             b = b +  i;
+             System.out.print(b);
+             c = i;
 
-            System.out.print(b);
-                c = b+i;
-        }
+
+      }
     }
 }
