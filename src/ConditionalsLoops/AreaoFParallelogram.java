9ccdd269834c9a44155398377ce10cef227061d7
N0tstgr <vikashkasaudhan321@gmail.com>
2026-01-24 09:56:25 +0530

    parallelogram

--- src/ConditionalsLoops/AreaoFParallelogram.java
+++ src/ConditionalsLoops/AreaoFParallelogram.java
@@ -1,7 +1,18 @@
 package ConditionalsLoops;
 
+import java.util.Scanner;
+
 public class AreaoFParallelogram {
     static void main() {
-        Scanner sc  = new
+        // Area of parallelogram = base * height
+        double areaofP = 0;
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the value of the base : ");
+        int b = sc.nextInt();
+        System.out.println("Enter the value of the height : ");
+        int h = sc.nextInt();
+        areaofP = b*h;
+        System.out.println(areaofP);
+
     }
 }
