e4f5b1ae8fc5b1589c08d3ee9600c708fea99616
N0tstgr <vikashkasaudhan321@gmail.com>
2026-01-25 12:49:50 +0530

    fibbonaciDone

---
+++ src/ConditionalsLoops/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package ConditionalsLoops;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    static void main() {
+        int firstnum = 0;
+        int secondnum = 1;
+        int nextnum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of the n : ");
+        int n = sc.nextInt();
+        for(int i = 0; i<=n; i++){
+            System.out.print(firstnum + " , ");
+            nextnum = firstnum + secondnum;
+            firstnum = secondnum;
+            secondnum = nextnum;
+        }
+
+    }
+}
