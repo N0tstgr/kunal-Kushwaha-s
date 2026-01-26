21380769235e4e61ff3a40ea71af6684dd465c62
N0tstgr <vikashkasaudhan321@gmail.com>
2026-01-26 18:25:10 +0530

    reAssignOriginal

--- src/com/methods/java/Scoping.java
+++ src/com/methods/java/Scoping.java
@@ -4,13 +4,21 @@ public class Scoping {
     static void main() {
         int a  = 95;
         int b = 56;
+        String name = "noot";
 //        System.out.println(marks);
         {
-            a= 89;
+            a= 89; //it will reassign the original ref variable to some other value;
+            System.out.println(a);
+            //already initialised outside the block in the same method hence you cannot initialised again
+            //but you can change the value as well
             int c = 34;
+            name = "luxxxxy";
+            //values initialised in this block, will remain in block
 
         }
-        System.out.println(c);
+        System.out.println(a);
+        System.out.println(name);
+//        System.out.println(c);//cannot be used inside the block
 
     }
     static void random(int marks){
