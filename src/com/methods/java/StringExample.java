458b269672f5943b87dce9cae00899ae88fccc2b
N0tstgr <vikashkasaudhan321@gmail.com>
2026-01-26 14:55:02 +0530

    parameter

---
+++ src/com/methods/java/StringExample.java
@@ -0,0 +1,22 @@
+package com.methods.java;
+
+public class StringExample {
+  public  static void main(String[] args) {
+//        String mail = greet();
+//        System.out.println(mail);
+
+        int ans = sum3(45,34);
+      System.out.println(ans);
+    }
+    //pass the value of the numbers when you are calling the method in main(_)
+    static int sum3(int a, int b){
+      int sum = a + b;
+      return sum;
+    }
+
+//    //return example
+//    static String greet(){
+//        String greeting = "Never Give up";
+//        return greeting;
+//    }
+}
