c91bf2e6ff1b7cc5e105a129b54d16720a3e6cb4
N0tstgr <vikashkasaudhan321@gmail.com>
2026-01-26 12:30:59 +0530

    greetings

---
+++ src/com/methods/java/Greeting.java
@@ -0,0 +1,18 @@
+package com.methods.java;
+import java.util.Scanner;
+public class Greeting {
+    static void main() {
+        greeting();
+
+    }
+//      System.out.println("Enter the number 1 : ");
+//    int a = sc.nextInt();
+//        System.out.println("Enter the number 2 : ");
+//    int b = sc.nextInt();
+//    int sum = a + b;
+//        System.out.println("The sum of the number is = "+ sum);
+
+    static void greeting(){
+        System.out.println("Hello vikash");
+    }
+}
