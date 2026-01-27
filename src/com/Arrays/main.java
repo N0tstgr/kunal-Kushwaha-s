a96ceefdb3c94ae9f132d425794b3ce8240658f3
N0tstgr <vikashkasaudhan321@gmail.com>
2026-01-27 16:20:59 +0530

    intro_array

---
+++ src/com/Arrays/main.java
@@ -0,0 +1,53 @@
+package com.Arrays;
+
+public class main {
+    static void main(String[] args) {
+        //store a roll no
+        int a = 93;
+
+        //Q store a person's name
+        String name = "dumpy";
+
+        //Q store 6 roll numbers;
+        int rollno1 = 34;
+        int rollno2 = 45;
+        int rollno3 = 32;
+
+        //Syntax
+        //datatype[a] variable_name = new datatype[size]
+//        int[]rno = new int[5];
+//        // or directly
+//        int[]rno4 = {4,6,2,7};
+
+
+        int[]ros; //declaration of array. ros is getting defined in the stack
+        ros = new int[5];  // internally==> [0,0,0,0,0]// initialisations actually here objects is being created in the (heap memory)
+        System.out.println(ros[2]);
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
+
+//        null a = 0; this is a literal
+//        String str = null;
+//        int num = null;
+
+        String[] arr5 = new String[6];
+        //internal working of object =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
