d635c1549a0414dc0f2b1e92305a9f8ac7300727
N0tstgr <vikashkasaudhan321@gmail.com>
2026-01-27 10:28:14 +0530

    overloading-done

--- src/com/methods/java/Overloading.java
+++ src/com/methods/java/Overloading.java
@@ -1,17 +1,30 @@
 package com.methods.java;
 
+import java.util.Arrays;
+
 public class Overloading {
     static void main() {
 //        fun(4);
         fun("not");
 //        same function but different arguments || datatype
-        sum(5,9);
+//        sum(5,9);
+        demo(9,76,5);
 
-        sum(4,6,7);
-        System.out.println(sum);
+        int ans = sum(4,6,78);
+        System.out.println(ans);
 
     }
-    static int  sum(int a, int b){
+
+    static void demo(int ...v){
+        System.out.println(Arrays.toString(v));
+    }
+    static void demo(String ...v){
+        System.out.println(Arrays.toString(v));
+    }
+
+
+    static int  sum(int a, int b)
+    {
         return a + b;
 
     }
